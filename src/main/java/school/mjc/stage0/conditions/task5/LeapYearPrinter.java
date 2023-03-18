01937bef9cb3ca240cfb607f8a56c95ed28b85c2
Yermek <yermek20@gmail.com>
2023-03-18 17:28:05 +0600

    Module4 Task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        switch (year%400) {
+            case 0:
+                System.out.println("leap");
+                break;
+            default:
+                switch (year%4) {
+                    case 0:
+                        switch (year%100) {
+                            case 0:
+                                System.out.println("not leap");
+                                break;
+                            default:
+                                System.out.println("leap");
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("not leap");
+                        break;
+                }
+                break;
+        }
     }
 }
